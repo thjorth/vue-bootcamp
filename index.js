6d7b4b7671695fc5159445c8e6247e825c435384
Thomas Hjorth <thomas.hjorth@valtech.com>
2018-12-10 21:13:20 +0100

    update

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
 var express = require('express');
 var app = express();
 var fs = require("fs");
+const fetch = require('node-fetch');
 
-app.use(express.static('www'));
+const API_BASE_URL = 'http://www.omdbapi.com/?apikey=4f31804c&';
 
-app.get('/listUsers', function (req, res) {
+
+
+app.use(express.static('ymdb/dist/'));
+
+app.get('/', function(req, res){
+    res.sendfile(__dirname + '/ymdb/dist/index.html', { root: __dirname + "/ymdb/dist" } );
+});
+
+app.get('/api/search', async function (req, res) {
+    if (!req.query.s) {
+        res.end('Error!');
+        return;
+    }
+
+    const url = `${API_BASE_URL}type=movie&s=${req.query.s}`;
+    const json = await (await fetch(url)).json();
+    res.end(JSON.stringify(json));
+});
+
+app.get('/api/details', async function (req, res) {
+    if (!req.query.id) {
+        res.end('Error!');
+        return;
+    }
+
+    const url = `${API_BASE_URL}i=${req.query.id}`;
+    const text = await (await fetch(url)).text();
+    res.end(text)
+});
+ 
+
+
+app.get('/api/listUsers', function (req, res) {
    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
       console.log( data );
       res.end( data );
