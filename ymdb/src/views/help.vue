a51856e09725fce5934cd6571aa03ac4babedafd
Thomas Hjorth <thomas.hjorth@valtech.com>
2018-12-13 00:31:58 +0100

    Added a bit more help

--- ymdb/src/views/help.vue
+++ ymdb/src/views/help.vue
@@ -2,6 +2,7 @@
   <div class="about text-left">
     <h2>Help!</h2>
     <p>Look in /src/main.js and see how the header component is imported and registered.</p>
+    <p>Look in /src/App.vue to see how the header component is inserted into the template and how prop values are passed down to the component</p>
     <p>Look at the /src/components/header.vue file to see how to access a prop</p>
     <p>
       <a href="https://vuejs.org/v2/guide/components-props.html" target="_blank">Read about props</a><br/>
