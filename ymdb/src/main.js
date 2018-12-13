f26980fc60d8cdc9af479b77360d23498c0268f6
Thomas Hjorth <thomas.hjorth@valtech.com>
2018-12-13 21:50:22 +0100

    Finalizing chapter 1

--- ymdb/src/main.js
+++ ymdb/src/main.js
@@ -5,13 +5,11 @@ import store from './store'
 
 // globally available components
 import header from './components/header';
-import search from './components/search';
 
 // styles
 import './scss/index.scss';
 
 Vue.component('c-header', header);
-Vue.component('c-search', search);
 
 Vue.config.productionTip = false
 
