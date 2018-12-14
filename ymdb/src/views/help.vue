d4df9ea6ec63efe080991bdb1e844fc5e3f231a8
Thomas Hjorth <thomas.hjorth@valtech.com>
2018-12-14 23:32:57 +0100

    task description and help for chapter 3

--- ymdb/src/views/help.vue
+++ ymdb/src/views/help.vue
@@ -2,22 +2,13 @@
   <div class="about text-left">
     <h2>Help!</h2>
     <p>
-      Read about <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/import" target="_blank">ES6 import statements</a>. 
-      Pay attention to the form <em>import { export1 , export2 } from "module-name";</em> as that allows us to import specific named exports from a 
-      module with multiple export statements.
+      Take a look at the <em>/src/views/Home.vue</em> view to see how they are structured.
     </p>
 
     <p>
-      A bit on <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function" target="_blank">async functions</a>. Note
-      that you use <em>await</em> when calling a function that is defined as <em>async</em>. You cannot use the <em>await</em> keyword unless doing so inside
-      a function that is defined as <em>async</em> itself.
+      Look at the other examples in <em>/src/router.js</em> to see how the paths are constructed. You can also <a href="https://router.vuejs.org/guide/" target="_blank">
+      read more about Vue router</a> and pay particular interest to the part about <a href="https://router.vuejs.org/guide/essentials/passing-props.html" target="_blank">
+      Passing Props To Route Components</a>
     </p>
-
-    <p>
-      Breakpoints and step debugging can be unpredictable inside <em>async</em> functions. A good old console.log statement might prove useful when inspecting
-      the results returned by the search.
-    </p>
-
-    <p>Look in /src/App.vue for an example of how create a v-for contruct.</p>
   </div>
 </template>
--- ymdb/src/views/task.vue
+++ ymdb/src/views/task.vue
@@ -2,17 +2,29 @@
     <div class="task text-left">
         <h2>Task</h2>
         <p>
-            It is time to hook up the search component to the movie service. Once that is done, expand
-            the search component to list the first 10 hits on the page.
+            Now we will create a view where we can see more about a particular movie. We will use the Vue router to handle the navigation,
+            and we will modify the search result so we can navigate to the detailed view of the movie. A view is just a Vue component, 
+            but it is useful to make the distinction and agree that views (or pages) are Vue components you can navigate to and regular components
+            are just building blocks you can use in the different views.
         </p>
         <ol>
-            <li>Import the search function from the movie service found in /src/services/movie.js</li>
-            <li>Add an empty (async) search method to search Vue component</li>
-            <li>Make sure that the search method on the component makes an async call to the search function that was imported from the movie service. The search function should be called with the search term as its sole parameter.</li>
-            <li>Examine the json structure that is returned by the call to the search function and render an element for each result in the markup</li>
+            <li>Create a new Vue component in /src/views/. Call it <em>movie.vue</em></li>
+            <li>Import the component into the router and add a rule allowing you to navigate to the newly created view/component. Make the router rule respond to <em>/movie</em></li>
+            <li>Add an <em>id</em> parameter to the route and make sure the route is configured to pass parameters in as props</li>
+            <li>Modify the result list from chapter 2 and insert a router-link in the template so you will navigate to <em>/movie/&lt;imdbId from the search result&gt;</em></li>
+            <li>Add an <em>id</em>prop to the movie.vue component. The router will pass the <em>id</em> from the path into the <em>id</em> prop in the movie.vue component</li>
+            <li>Import the <em>getDetails</em> function from the movie service into the component</li>
+            <li>Add a <em>mounted</em> hook in the component. This will be called whenever the movie component is mounted in the DOM.</li>
             <li>
-                <strong>For bonus points:</strong> Create a searchResultItem.vue component that can render the markup for a single item in the list. Then modify the search
-                component to render out one of these item components per item in the result.
+                Inside the mounted hook, you should make an async call to the <em>getDetails</em> function and supply the <em>id</em> prop as a parameter. This will fetch
+                details about the movie and return it as an object. Use console.log to log it out into the console so you can inspect the contents and structure of it
+            </li>
+            <li>
+                Add a data property to the view and once you get the object back from <em>getDetails</em> in the mounted hook, assign the object to that data attribute.
+                Now the contents of that object are available to use in the template.
+            </li>
+            <li>
+                Add markup to the template and insert references to the properties in the data object so you can display the details for the movie.
             </li>
         </ol>
     </div>
