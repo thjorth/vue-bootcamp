0a561a04c40a3c070bf6b7bee70b1cfb6f00063b
Thomas Hjorth <thomas.hjorth@valtech.com>
2018-12-18 20:59:21 +0100

    Added task description and help to chapter 6

--- ymdb/src/views/help.vue
+++ ymdb/src/views/help.vue
@@ -2,8 +2,8 @@
   <div class="about text-left">
     <h2>Help!</h2>
     <p>
-      If you are interested you can look at /src/services/movie.js to check the inner workings of the service and how it calls the API that is 
-      hooked up to the movie database and the ratings database.
+      Read about <a href="https://vuex.vuejs.org/guide/getters.html" target="_blank">getters and the mapGetters function</a><br/>
+      Read about <a href="https://vuex.vuejs.org/guide/mutations.html" target="_blank">mutations and the mapMutations function</a><br/>
     </p>
   </div>
 </template>
--- ymdb/src/views/task.vue
+++ ymdb/src/views/task.vue
@@ -2,22 +2,27 @@
     <div class="task text-left">
         <h2>Task</h2>
         <p>
-            It is time to add some ratings to our movie detail view. For that purpose, a couple of useful functions have been added to the movie service. 
-            <em>getRatings</em> will fetch the ratings for a given movie while <em>postRating</em> will post a rating for a given movie. Ratings ranging
-            from 0 to 10 (both values included) are accepted by the server.
+            Let's make sure that our search results are not lost when click the back button from a movie view. To do that we will use vuex to persist
+            the search result when navigating between the different views. Vuex is also what you use whenever you need to share data across multiple
+            components.
         </p>
         <ol>
-            <li>Import the two new methods from /src/services/movie.js. Use the<br/><em><strong>import</strong> { method1, method2 } <strong>from</strong> './path/to/file.js';</em><br/>form.</li>
+            <li>Look at the /src/store.js file. This store was created by vue-cli - I have just added a few things to it</li>
+            <li>The store is already added to Vue so inside every Vue component the store can be accessed as <em>this.$store</em></li>
             <li>
-                In the <em>mounted</em> hook add an async call to <em>getRatings</em> and console log the returned value out. <strong>Hint:</strong>
-                The ratings database is almost empty but some disturbed individual has already put a bunch of votes on The House That Jack Built so 
-                use that title for testing purposes.
+                Modify the search function so it calls the commit method on the store and invokes the <em>setSearchResult</em> mutation
+                and supplies the array holding the search results as a parameter.
             </li>
-            <li>Create a UI that can render out the ratings on the movie view. You can do it as a separate component ot just simply add it to the movie.vue component template.</li>
-            <li>The create a UI that can be used for selecting your own rating including a button to submit the rating.</li>
-            <li>Create a <em>submitRating</em> method on the Vue component and hook the button to submit ratings up to it</li>
-            <li>In that method make an async call to the <em>postRating</em> method from the movie service. This method takes the id of the movie and the rating as an integer as parameters</li>
-            <li>Once done, you can start rating different movies.</li>
+            <li>
+                Use <em>mapGetters</em> to map the <em>getSearchResult</em> getter on the store into the <em>computed</em> property on 
+                the Vue component. This will make getSearchResult a reactive getter that you can bind your ui to.
+            </li>
+            <li>
+                <strong>Bonus points:</strong> use <em>mapMutations</em> from <em>vuex</em> to map the setter into the methods property on
+                the Vue component. Now you can just call that method when setting the search result instead of having to use 
+                <em>this.$store.commit('setSearchResult', result)</em>
+            </li>
+            <li>Modify the UI so the list of search results is rendered based on the mapped getter instead of the data property</li>
         </ol>
     </div>
 </template>
